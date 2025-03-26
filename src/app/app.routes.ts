9f90fa995df56184c0f500a213552759bb3f6fbc
Diegoweh <diegocelaia@outlook.com>
2025-03-25 22:10:22 -0700

    produc-detail page added and finished also account page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
   {
     path: '',
     redirectTo: 'tabs',
@@ -9,23 +8,7 @@ export const routes: Routes = [
   },
   {
     path: 'tabs',
-    loadComponent: () => import('./pages/tabs/tabs.page').then( m => m.TabsPage),
-    children: [
-      {
-        path: '',
-        redirectTo: '/tabs/home',
-        pathMatch: 'full'
-      },     
-      {
-          path: 'home',
-          loadComponent: () => import('./pages/tabs/home/home.page').then( m => m.HomePage)
-      },
-      {
-          path: 'account',
-          loadComponent: () => import('./pages/tabs/account/account.page').then( m => m.AccountPage)
-      },
-      
-    ]
+    loadChildren: () =>
+      import('./pages/tabs/tabs.routes').then((m) => m.routes),
   },
-  
 ];
--- src/app/pages/tabs/account/account.page.ts
+++ src/app/pages/tabs/account/account.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonAvatar, IonItem, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
   styleUrls: ['./account.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonList, IonItem, IonAvatar, IonLabel, IonIcon, IonTabButton, IonTabBar, IonTabs, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class AccountPage implements OnInit {
 
